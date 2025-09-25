a8be8e0a538d3ca1a826b06d40a2be1fb589b97e
Dayo <iamdayoaderinto@gmail.com>
2025-09-25 01:57:55 +0100

    Modify DB Engine

--- infra/modules/sg/main.tf
+++ infra/modules/sg/main.tf
@@ -74,9 +74,17 @@ resource "aws_security_group" "internal_alb_sg" {
         cidr_blocks      = ["0.0.0.0/0"]
     }
     ingress {
-        description      = "Allow HTTP"
+        description      = "Allow HTTPS"
         from_port        = 443
         to_port          = 443
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description      = "Allow SSH"
+        from_port        = 22
+        to_port          = 22
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
     }
